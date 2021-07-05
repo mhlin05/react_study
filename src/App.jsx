6f8add68af421edd611155cc4d2a7d4ddc017f35
linminghao <172755874@qq.com>
2021-07-05 16:17:00 +0800

    代理配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React, { Component } from 'react'
-
+import Axios from 'axios'
 export default class App extends Component {
+  getStudentData = () => {
+    Axios.get('http://localhost:3000/api1/students').then(
+      (Response) => {
+        console.log('成功了', Response.data)
+      },
+      (error) => {
+        console.log('失败了')
+      }
+    )
+    // const { data } = await Axios.get('http://localhost:3000/students')
+    // console.log(data)
+  }
+  getCarData = () => {
+    Axios.get('http://localhost:3000/api2/cars').then(
+      (Response) => {
+        console.log('成功了', Response.data)
+      },
+      (error) => {
+        console.log('失败了')
+      }
+    )
+  }
   render() {
-    return <div>app...</div>
+    return (
+      <div>
+        <button onClick={this.getStudentData}>点我学生获取</button>
+        <button onClick={this.getCarData}>点我汽车获取</button>
+      </div>
+    )
   }
 }
